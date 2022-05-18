4f090f58d198f6050ec247deb46f3863e82640f8
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-05-18 10:27:19 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -9,11 +9,12 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(response => response.json())
     .then(dogBreedsObject => {
       addBreeds(dogBreedsObject);
-    });
+  });
+
+  document.getElementById('breed-dropdown').addEventListener('change', filterBreedsByLetter);
 
   function addBreeds(dogBreedsObject) {
     const ul = document.getElementById('dog-breeds');
-    // console.log(dogBreedsObject.message);
     Object.keys(dogBreedsObject.message).forEach(function (key) {
       const li = document.createElement('li');
       li.innerHTML = `${key}`;
@@ -22,12 +23,17 @@ document.addEventListener('DOMContentLoaded', () => {
         li.style.color = 'red';
       });
     });
-    const breedDropDown = document.getElementById('breed-dropdown');
-    breedDropDown.addEventListener('change', (event) => {
-      console.log(event.target.value);
+  }
+
+  function filterBreedsByLetter(event) {
+    const breedsListItems = document.querySelectorAll('li');
+      breedsListItems.forEach((breed) => {
+        if(!breed.innerHTML.startsWith(event.target.value)){
+          breed.style.display = 'none';
+        } else {
+          breed.style.display = 'list-item';
+        }
     });
-    const li = document.querySelectorAll('li')
-    console.log(li);
   }
 
   function addDogImages(dogsObject) {
@@ -39,24 +45,6 @@ document.addEventListener('DOMContentLoaded', () => {
       img.width = '300';
       img.height = '300';
       div.appendChild(img);
-      // console.log(img);
-      // console.log(dogsObject.message[key]);
     });
-    // console.log(dogsObject.message[0]);
   }
 });
-
-
-const array = ['affenpinscher',
-  'african',
-  'airedale',
-  'basenji',
-  'beagle',
-  'cattledog',
-  'chihuahua',
-  'dachshund',
-  'dalmatian',
-  'dane']
-
-const startsWithN = array.filter((breed) => breed.startsWith('d'));
-// console.log(startsWithN);
